0deec07aad7284e22b0c9d2fb28633360d705e64
Annamária Soós <annasoos94@gmail.com>
2021-10-01 13:07:35 +0200

    add explanation comments to class component practice file

--- frontend/src/components/Class_Components_Practice/Featured_Class.tsx
+++ frontend/src/components/Class_Components_Practice/Featured_Class.tsx
@@ -19,8 +19,10 @@ export class MyComponent extends React.Component<any, FeaturedClassStateType> { 
 	/*
 	componentDidMount() {
 		getData("http://localhost:8080/api/find-a-job").then(data => this.setState({jobs: data}, () => {console.log(this.state)}));
-	}  //React updates state asynchronously, if you want to see the updated state when it's done, you should use the callback function of setState which gets fired as soon as the state gets updated
+	}  
 	*/
+	//React updates state asynchronously, if you want to see the updated state when it's done, you should use the callback function of setState which gets fired as soon as the state gets updated
+	
 
 	componentDidMount() {
 		getData("http://localhost:8080/api/find-a-job").then(data => {
@@ -50,12 +52,39 @@ export class MyComponent extends React.Component<any, FeaturedClassStateType> { 
 
 
 /*
-When implementing the constructor() function inside a React component, super() is a requirement. Keep in mind that your MyComponent component is extending or borrowing functionality from the React.Component base class.
-This base class has a constructor() function of its own that has some code inside of it, to setup our React component for us.
+When implementing the constructor() function inside a React component, super() is a requirement. MyComponent component is extending or borrowing functionality from the React.Component base class. This base class has a constructor() function of its own to setup our React component for us.
+When we define a constructor() function inside our MyComponent class, we are overriding or replacing the constructor() function that is inside the React.Component class, but we still need to ensure that all the setup code inside of this constructor() function still gets called. super(props) is a reference to the parents constructor() function. The reason for defining this constructor() funciton is to initialize our state object.
+*/
 
-When we define a constructor() function inside our MyComponent class, we are essentially, overriding or replacing the constructor() function that is inside the React.Component class, but we still need to ensure that all the setup code inside of this constructor() function still gets called.
 
-So to ensure that the React.Component’s constructor() function gets called, we call super(props). super(props) is a reference to the parents constructor() function, that’s all it is.
 
-The entire reason for defining this constructor() funciton is to initialize our state object.
+// ----------------FUNCTIONAL VERSION -------------------
+
+/* 
+export const Featured = () => {
+
+	const[data, setData] = useState<IJob[]>([]);
+
+	useEffect(() => {	
+		getData("http://localhost:8080/api/find-a-job").then(setData);
+	}, []);
+
+	const filtered:IJob[] = data.slice(0, 3);
+
+	return (
+		<FeaturedSection>
+			<Search />
+
+			<Title>Featured Positions</Title>
+			<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis aliquid provident quasi possimus autem!</Text>
+
+			<CardContainer>
+				{filtered.map((card: IJob, index: number) => (<Card key={index} cardElement={card} position={index} /> ))}
+				<BlueLine />
+			</CardContainer>
+
+		</FeaturedSection>
+	);
+};
+
 */
--- frontend/src/components/Featured/Featured.tsx
+++ frontend/src/components/Featured/Featured.tsx
@@ -41,36 +41,3 @@ export class Featured extends React.Component<any, FeaturedClassStateType> {
 		)
   }
 }
-
-
-
-// ----------------FUNCTIONAL VERSION -------------------
-
-/* 
-export const Featured = () => {
-
-	const[data, setData] = useState<IJob[]>([]);
-
-	useEffect(() => {	
-		getData("http://localhost:8080/api/find-a-job").then(setData);
-	}, []);
-
-	const filtered:IJob[] = data.slice(0, 3);
-
-	return (
-		<FeaturedSection>
-			<Search />
-
-			<Title>Featured Positions</Title>
-			<Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis aliquid provident quasi possimus autem!</Text>
-
-			<CardContainer>
-				{filtered.map((card: IJob, index: number) => (<Card key={index} cardElement={card} position={index} /> ))}
-				<BlueLine />
-			</CardContainer>
-
-		</FeaturedSection>
-	);
-};
-
-*/
