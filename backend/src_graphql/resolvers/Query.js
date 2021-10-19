65ecd2c32d16d40a00bcc42aa4da40c8688e28e3
Annamária Soós <annasoos94@gmail.com>
2021-10-19 13:39:50 +0200

    add filter to feed query

--- backend/src_graphql/resolvers/Query.js
+++ backend/src_graphql/resolvers/Query.js
@@ -1,5 +1,24 @@
-function feed(parent, args, context, info) {
-  return context.prisma.job.findMany()
+async function feed(parent, args, context, info) {
+  const where = args.filter
+    ? {
+      OR: [
+        { position: { contains: args.filter } },
+				{ level: { contains: args.filter } },
+				{ location: { contains: args.filter } },
+				{ company: { contains: args.filter } },
+				{ skills: { contains: args.filter } },
+        { description: { contains: args.filter } },
+      ],
+    }
+    : {}
+
+  const jobs = await context.prisma.job.findMany({
+    where,
+  })
+
+	//If no filter string is provided, then the where object will be just an empty object and no filtering conditions will be applied by Prisma Client when it returns the response for the links query.
+
+  return jobs
 }
 
 module.exports = {
