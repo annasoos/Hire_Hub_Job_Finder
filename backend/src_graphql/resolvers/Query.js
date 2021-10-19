deea5af30d708cc46525469dd0807634a7d2e5f4
Annamária Soós <annasoos94@gmail.com>
2021-10-19 15:18:04 +0200

    add limit-offset pagination to feed query

--- backend/src_graphql/resolvers/Query.js
+++ backend/src_graphql/resolvers/Query.js
@@ -14,9 +14,14 @@ async function feed(parent, args, context, info) {
 
   const jobs = await context.prisma.job.findMany({
     where,
+		skip: args.skip,
+		take: args.take,
   })
 
 	//If no filter string is provided, then the where object will be just an empty object and no filtering conditions will be applied by Prisma Client when it returns the response for the links query.
+
+	//Limit-Offset Pagination: Request a specific chunk of the list by providing the indices of the items to be retrieved (in fact, you’re mostly providing the start index (offset) as well as a count of items to be retrieved (limit)).
+	//Limit and offset have different names in the Prisma API: The limit is called take, meaning you’re “taking” x elements after a provided start index. The start index is called skip, since you’re skipping that many elements in the list before collecting the items to be returned. If skip is not provided, it’s 0 by default. The pagination then always starts from the beginning of the list.
 
   return jobs
 }
