83e27089bfe9637729cd0f8da2ef36e29c3b4908
Annamária Soós <annasoos94@gmail.com>
2021-11-25 14:25:54 +0100

    add form and submit notification to fake newsletter subscription

--- frontend/src/components/Newsletter/Newsletter.tsx
+++ frontend/src/components/Newsletter/Newsletter.tsx
@@ -1,24 +1,45 @@
-import { Input } from "antd";
+import { Input, Form } from "antd";
+import { openNotificationWithIcon } from "../../utils/functions/Notification";
 import { NewsletterContainer, NewsletterTitle, Button, NewsletterText } from "./Newsletter.style";
 
 export const Newsletter = () => {
-  return (
+	const [form] = Form.useForm();
+	const submit = () => {
+		openNotificationWithIcon(
+			"success",
+			"Yaaay!",
+			"You subscribed to our weekly newsletter. Please check your emails for our confirmation."
+		);
+	}
+  
+	return (
     <NewsletterContainer>
       <NewsletterTitle> Be up to date with every opportunity </NewsletterTitle>
       <NewsletterText>
         Don't miss out on a single job offer by subscribing to our newsletter
         and join the Hire Hub community!
       </NewsletterText>
-      <Input
-        className="input"
-        placeholder="Add your e-mail adress"
-        allowClear
-        type="email"
-        name="email"
-        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2, 4}$"
-        autoComplete="on"
-      ></Input>
-      <Button>Subscribe</Button>
+      <Form
+        className="newsletterForm"
+        form={form}
+        onFinish={submit}
+        colon={false}
+        autoComplete="off"
+      >
+        <Form.Item
+					className="input"
+          name="email"
+          required
+          rules={[{ required: true, message: "Please provide a valid email address", type: "email" }]}
+        >
+          <Input className="input" allowClear placeholder="Add your e-mail adress" />
+        </Form.Item>
+        <Form.Item>
+          <Button>
+            Subscribe
+          </Button>
+        </Form.Item>
+      </Form>
     </NewsletterContainer>
   );
 };
