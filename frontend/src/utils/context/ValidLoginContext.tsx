73cc2159e34c28c4e06ed57c42fcfe6de24f65ff
Annamária Soós <annasoos94@gmail.com>
2021-11-29 14:19:21 +0100

    refactor validLoginContext

--- frontend/src/utils/context/ValidLoginContext.tsx
+++ frontend/src/utils/context/ValidLoginContext.tsx
@@ -1,14 +1,11 @@
 import { createContext, useState } from 'react';
 import { ValidLoginContextType, ContextProviderProps } from "../types/ValidLoginContextTypes";
 
-export const ValidLoginContext = createContext<ValidLoginContextType>({
-		validLogin: false,
-		setValidLogin: () => { }
-});
+export const ValidLoginContext = createContext<ValidLoginContextType>({} as ValidLoginContextType);
 
 export const ValidLoginContextProvider = ({children}: ContextProviderProps) => {
 	
-	const [validLogin, setValidLogin] = useState(false)
+	const [validLogin, setValidLogin] = useState(sessionStorage.getItem("user logged in") !== null)
 	
 	return (
 	<ValidLoginContext.Provider value={{validLogin, setValidLogin}}>
